9ab630d10158936da88ea4d5c3db55caaff07136
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-06-12 10:48:27 -0700

    Added function to add any wxsizer to vertical box sizer in timeline window.

--- src/timeline-track-editor.cpp
+++ src/timeline-track-editor.cpp
@@ -99,9 +99,11 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 	int spaceBlockSize = 100;
 	timeFrame->AddSpacerBlock(spaceBlockSize);
 	
-	//add text to the timeline window at certain point
-	wxPoint textPoint; textPoint.x = 100; textPoint.y = 100;
-	timeFrame->AddText("Track for someVarToChange",textPoint);
+	//add text to the timeline window before track to indicate what variable the track manipulates
+	wxBoxSizer* hboxText = new wxBoxSizer(wxHORIZONTAL);
+	wxStaticText *text = new wxStaticText(timeFrame->GetTimelineWindow(), wxID_ANY, wxT("Track for someVarToChange"),wxDefaultPosition );
+	hboxText->Add(text);
+	timeFrame->AddBoxSizer(hboxText);
 	
 	//add track to time frame
 	timeFrame->AddTrack(track1,space);
