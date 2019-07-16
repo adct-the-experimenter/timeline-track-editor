9dae358eef1676d21e49949b1bab3e5ec62a62c3
Pablo Camacho <bringerofawesomefood@gmail.com>
2019-07-15 23:06:55 -0700

    Can now load .gg,.flac files.

--- src/stereo-audio-track.cpp
+++ src/stereo-audio-track.cpp
@@ -223,7 +223,7 @@ void StereoAudioTrack::BrowseForInputAudioFile()
 {
 	if(audio_data_stream.GetSize() == 0)
 	{
-		wxFileDialog fileDlg(this, _("Choose the WAV file"), wxEmptyString, wxEmptyString, _("WAV file|*.wav|All files|*.*"));
+		wxFileDialog fileDlg(this, _("Choose the WAV,FLAC,OGG file"), wxEmptyString, wxEmptyString, _("WAV file|*.wav|FLAC file|*.flac|OGG file|*.ogg|All files|*.*"));
 		if (fileDlg.ShowModal() == wxID_OK)
 		{
 			wxString path = fileDlg.GetPath();
